89c0d3b1fa31d89df4284cd17381bc19c68090b5
League <kierancronin213@gmail.com>
2023-02-03 18:26:43 -0800

    ddkjhh

--- src/_04_netflix/NetflixYes.java
+++ src/_04_netflix/NetflixYes.java
@@ -1,5 +1,12 @@
 package _04_netflix;
 
 public class NetflixYes {
-
+	public static void main(){
+		Movie Marmaduke = new Movie("Marmaduke", 1);
+		Movie Morbius = new Movie("Morbuis", 2);
+		Movie Eternals = new Movie("Eternals", 3);
+		Movie Minions = new Movie("Minions: The Rise of Gru", 5);
+		Movie PacificRim = new Movie("Pacific Rim", 0);
+	}
 }
+
